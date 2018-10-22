2f9420d5db80533f9e168bdaf7e18133279c3cfe
Mythyndian <zawistowskimaciej1@gmail.com>
2018-10-22 14:21:41 +0200

    Add files via upload

---
+++ zad17b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+	{
+	int n1,n10,n100;
+	printf("Wprowadz liczbe trzycyfrowa:");
+	scanf("%1d%1d%1d",&n1,&n100,&n100);
+
+	printf("%d%d%d",n1,n10,n100);
+	return 0;
+	}
