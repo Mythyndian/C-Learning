957fa952eb7ae8ed74f798bc2305dd3ae8920a43
Mythyndian <zawistowskimaciej1@gmail.com>
2018-12-02 12:00:40 +0100

    Update zad7l4.c

--- zad7l4.c
+++ zad7l4.c
@@ -2,19 +2,24 @@
 
 int power(int a, int n);
 
+double negative_power(int a, int n);
+
 int main(void)
 	{
 		int a,n;
 		printf("Podaj podstawe potegi a i wykladnik (n>0) po spacji: \n");
 		scanf("%d %d",&a,&n);
-		printf("%d^%d = %d",a,n,power(a,n));
 		
+		if(n >= 0)
+			printf("%d^%d = %d",a,n,power(a,n));
+		else
+			printf("%d^%d = %lf",a,n,negative_power(a,n));
 		return 0;
 	}
 
 int power(int a, int n)
 	{
-	int wynik;
+	double wynik;
 	if(n > 0)
 		{
 		wynik = 1;
@@ -23,14 +28,15 @@ int power(int a, int n)
 		return wynik;
 		}
 	else
-		if(n == 0)
-			wynik = 1;
-	else
-		{
-		wynik = 1;
-		for(int i = 0; i>n; i--)
-		wynik *=1/a;
-		return wynik;
-		}
-
+		return 1;
 	}
+
+double negative_power(int a, int n)
+{
+	double wynik = 1;
+	for(n; n<0; n++)
+	{
+		wynik = wynik * 1/(double)a;
+	}
+	return wynik;
+}
