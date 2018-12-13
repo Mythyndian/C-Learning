625c2f4b19282e8bb4da3cc95417f465ae2d56c6
Mythyndian <zawistowskimaciej1@gmail.com>
2018-12-13 17:40:51 +0100

    Update zad2L5.c

--- zad2L5.c
+++ zad2L5.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+void bigger_number(double *a,dobule *b);
+int main(void)
+{
+  double x,y
+  printf("Podaj dwie liczby:");
+  scanf("%lf %lf",x,y);
+  bigger_number(&x,&y);
+  printf("x: %f y: %f",x,y);
+  
+return 0;
+}
+void bigger_number(double *a,dobule *b)
+{
+  if(*a > *b)
+    *b = *a;
+  else
+    a* = *b;
 
+}
