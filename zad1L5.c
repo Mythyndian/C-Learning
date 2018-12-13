c14f537a50c018a34fd9924ab8b71e12cf146e2a
Mythyndian <zawistowskimaciej1@gmail.com>
2018-12-13 16:02:43 +0100

    Create zad1L5.c

---
+++ zad1L5.c
@@ -0,0 +1,19 @@
+  #include <stdio.h>
+
+int main()
+{
+    char c = 'c';   char *ch = &c;
+    float f = 1.5;  float *fl = &f;
+    int i = 1;  int *l = &i;
+    double d = 12.1; double *p_d = &d;
+
+
+    printf("char: %c\nfloat: %f\nint: %d\ndouble: %f\n\n",c,f,i,d);
+    printf("char: %p\nfloat: %p\nint: %p\ndouble: %p\n\n",c,f,i,d);
+
+    printf("char: %c\nfloat: %f\nint: %d\ndouble: %f\n\n",*ch,*fl,*l,*p_d);
+    printf("char: %p\nfloat: %p\nint: %p\ndouble: %p\n\n",*ch,*fl,*l,*p_d);
+    printf("char jeszcze raz : %c",ch);
+        return 0;
+}
+    
