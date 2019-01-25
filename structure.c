1a33916793ca705feba81424459a3ecc53855996
Mythyndian <zawistowskimaciej1@gmail.com>
2019-01-25 14:05:27 +0100

    Create structure.c

---
+++ structure.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+typedef struct product{
+    int id;
+    char name[15];
+    double weight;
+    double price;
+}PRODUCT ;
+void  wprowadzwprowadz(PRODUCT *P,char *t,int size);
+int main(void) {
+   PRODUCT mleko,jajka,platki;
+	PRODUCT *p;
+	p = &jajka;
+	wprowadz(&p,jajka.name,15);
+	
+
+	printf("name: %s %d %lf %lf",jajka.name,jajka.id,jajka.price,jajka.weight);
+	return 0;
+
+}
+void wprowadz(PRODUCT *P,char *t,int size){
+	printf("Podaj id: ");scanf("%d",&P->id);		
+	printf("Podaj name: ");fgets(t,size,stdin);
+	printf("Podaj weight: ");scanf("%lf",&P->weight);
+	printf("Podaj price: ");scanf("%lf",&P->price);
+	
+}
+
